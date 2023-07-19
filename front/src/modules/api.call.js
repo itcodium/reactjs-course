fce0ce00d7562efc27b5da5ad05b893f353f5e16
unknown <you@example.com>
2023-07-19 19:47:45 -0300

    fix api call

--- front/src/modules/api.call.js
+++ front/src/modules/api.call.js
@@ -1,6 +1,6 @@
 export default function ApiCall(url, params = {}, body) {
     console.log("ApiCall: ", url)
-    const isFlickrUrl = !!(url.indexOf('www.flickr.com/services'))
+    const isFlickrUrl = (url.indexOf('www.flickr.com/services')>-1)
     const token = localStorage.getItem('token');
     const id_usuario = localStorage.getItem('id_usuario');
 
@@ -13,6 +13,7 @@ export default function ApiCall(url, params = {}, body) {
         body: body ? JSON.stringify(body) : null,
         headers: params.headers
     } : null;
+    console.log("fetchParams", {fetchParams})
     return fetch(url, fetchParams)
         .then(response => {
             return response.json();
--- front/src/modules/flickr/views/FlickrContainer.js
+++ front/src/modules/flickr/views/FlickrContainer.js
@@ -7,27 +7,42 @@ import Typography from '@mui/material/Typography';
 import CircularProgress from '@mui/material/CircularProgress';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
 
 const FlickrContainer = () => {
     const dispatch = useDispatch();
     const status = useSelector(state => state.flickr.status);
-    const error = useSelector(state => state.flickr.photos?.error);
-    const data = useSelector(state => state.flickr.data.photos.photo);
+    const error = useSelector(state => state.flickr);
+    const data = useSelector(state => state.flickr.data?.photos?.photo);
     useEffect(() => {
         dispatch(fetchPhotos())
-        console.log("init -16");
+        console.log("init -16", error);
     }, [dispatch])
     return (
         <>
             {status === STATUS.SUCCESS ?
-                <ImageList  cols={3}>
-                    {data.map((item) => (
-                        <ImageListItem key={item.img}>
+                <ImageList cols={3}>
+                    {data.map((item, index) => (
+                        <ImageListItem key={index}>
                             <img
-                                src={'http://farm'+item.farm+'.staticflickr.com/'+item.server+'/'+item.id+'_'+ item.secret+'_z.jpg'}
-                                srcSet={'http://farm'+item.farm+'.staticflickr.com/'+item.server+'/'+item.id+'_'+ item.secret+'_w.jpg'}
+                                src={'http://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_z.jpg'}
+                                srcSet={'http://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_w.jpg'}
                                 alt={item.title}
                                 loading="lazy"
+                            />
+                            <ImageListItemBar
+                                title={item.title}
+                                subtitle={item.author}
+                                actionIcon={
+                                    <IconButton
+                                        sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                                        aria-label={`info about ${item.title}`}
+                                    >
+                                        <InfoIcon />
+                                    </IconButton>
+                                }
                             />
                         </ImageListItem>
                     ))}
